6e80593deea7709852ced5a4ef9466009f861497
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-15 15:43:58 -0500

    Add daisyui plugin and update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundColor: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: ['winter', 'dracula'],
+  },
+} satisfies Config
