d6b7615f100edf547579f3e628f405dfced77962
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-16 16:56:46 -0500

    Implement NavLinks component with dynamic link rendering

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link'
+
+type Links = {
+  href: string
+  label: string
+}
+
+const links: Links[] = [
+  { href: '/chat', label: 'chat' },
+  { href: '/tours', label: 'tours' },
+  { href: '/tours/new', label: 'new tour' },
+  { href: '/profile', label: 'profile' },
+]
+
 export default function NavLinks() {
   return (
-    <div>NavLinks</div>
+    <ul className='menu text-base-content'>
+      {links.map((link) => {
+        return (
+          <li key={link.label} className='capitalize'>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
