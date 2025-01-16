3d5f08cc953e4fe5036eeb76e989c20099961a5b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-16 16:47:58 -0500

    Refactor Sidebar component to include SidebarHeader, NavLinks, and MemberProfile with loading state

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,19 @@
+import { MemberProfile, NavLinks, SidebarHeader } from '@/components'
+import { Suspense } from 'react'
+
 export default function Sidebar() {
-  return <div className='text-6xl'>Sidebar</div>
+  return (
+    <div className='px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]'>
+      {/* First Row */}
+      <SidebarHeader />
+
+      {/* Second Row */}
+      <NavLinks />
+
+      {/* Third Row */}
+      <Suspense fallback={<p>loading...</p>}>
+        <MemberProfile />
+      </Suspense>
+    </div>
+  )
 }
