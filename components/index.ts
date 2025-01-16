3912f93ce8a753bdedcd3973519f041f908eb131
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-16 17:23:47 -0500

    Export Spinner and ThemeToggle components in index file

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
+export { default as Spinner } from './Spinner'
 export { default as MemberProfile } from './MemberProfile'
 export { default as NavLinks } from './NavLinks'
 export { default as Sidebar } from './Sidebar'
 export { default as SidebarHeader } from './SidebarHeader'
+export { default as ThemeToggle } from './ThemeToggle'
