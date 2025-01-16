e137bc7ce7eeb2feda4299ca792b137e8ed7cea0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-16 17:50:02 -0500

    Export Chat component from index file for easier access

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export { default as Chat } from './Chat'
 export { default as Spinner } from './Spinner'
 export { default as MemberProfile } from './MemberProfile'
 export { default as NavLinks } from './NavLinks'
