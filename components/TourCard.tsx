15f1c11f29adfe82c7fe3be41dcf6dd8dae2c345
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 16:02:48 -0500

    Refactor TourCard component to accept tour data as props and display city and country with a link

--- components/TourCard.tsx
+++ components/TourCard.tsx
@@ -1,3 +1,19 @@
-export default function TourCard() {
-  return <div>TourCard</div>
+import type { TourData } from '@/utils/types'
+import Link from 'next/link'
+
+export default function TourCard({ tour }: { tour: TourData }) {
+  const { city, country, id } = tour
+
+  return (
+    <Link
+      href={`/tours/${id}`}
+      className='card card-compact rounded-xl bg-base-100'
+    >
+      <div className='card-body items-center text-center'>
+        <h2 className='card-title text-center'>
+          {city}, {country}
+        </h2>
+      </div>
+    </Link>
+  )
 }
