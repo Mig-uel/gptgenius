2735cb838546a74197f1a6a1356d0f6f3a44d2e8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 12:47:59 -0500

    Add tour generation functionality with loading state in NewTour component

--- components/NewTour.tsx
+++ components/NewTour.tsx
@@ -1,14 +1,42 @@
 'use client'
 
+import { Spinner } from '@/components'
+import { generateTourResponse } from '@/utils/actions'
+import type { Tour } from '@/utils/types'
+import { useMutation } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 import TourInfo from './TourInfo'
 
 export default function NewTour() {
+  const {
+    data: tour,
+    isPending,
+    mutate,
+  } = useMutation({
+    mutationFn: async (destination: Tour) => {
+      const newTour = await generateTourResponse(destination)
+
+      if (newTour) {
+        return newTour
+      }
+
+      toast.error('No matching city found...')
+      return null
+    },
+  })
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget)
 
-    const destination = Object.fromEntries(formData)
+    const destination = Object.fromEntries(formData) as Tour
+
+    mutate(destination)
+  }
+
+  if (isPending) {
+    return <Spinner />
   }
 
   return (
@@ -33,15 +61,17 @@ export default function NewTour() {
             required
           />
 
-          <button className='btn btn-primary join-item' type='submit'>
-            Generate Tour
+          <button
+            disabled={isPending}
+            className='btn btn-primary join-item'
+            type='submit'
+          >
+            {isPending ? <Spinner className='loading-lg' /> : 'Generate Tour'}
           </button>
         </div>
       </form>
 
-      <div className='mt-16'>
-        <TourInfo />
-      </div>
+      <div className='mt-16'>{tour ? <TourInfo tour={tour} /> : null}</div>
     </>
   )
 }
