ecfbcb9e8db5f6a3ca14aba5eb6149d38052ffb2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 11:00:49 -0500

    Refactor generateChatResponse to accept an array of chat messages and handle errors gracefully

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,25 +5,23 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-export const generateChatResponse = async (chatMessage: string) => {
-  const response = await openai.chat.completions.create({
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a helpful assistant with a dialect from the 70s',
-      },
-      {
-        role: 'user',
-        content: chatMessage,
-      },
-    ],
+export const generateChatResponse = async (chatMessages: string[]) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful assistant with a dialect from the 70s',
+        },
+        ...chatMessages,
+      ],
 
-    model: 'gpt-3.5-turbo',
-    temperature: 0,
-  })
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+    })
 
-  console.log(response.choices[0])
-  console.log(response)
-
-  return 'awesome'
+    return response.choices[0].message
+  } catch (error) {
+    return null
+  }
 }
