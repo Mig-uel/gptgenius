a630a90710d1cbff4e664a690ee8006937d8c8f7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 16:14:07 -0500

    Update queryKey in prefetchQuery to include an empty string for ToursPage

--- app/(dashboard)/tours/page.tsx
+++ app/(dashboard)/tours/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
   const queryClient = new QueryClient()
 
   await queryClient.prefetchQuery({
-    queryKey: ['tours'],
+    queryKey: ['tours', ''],
 
     queryFn: () => getAllTours(),
   })
