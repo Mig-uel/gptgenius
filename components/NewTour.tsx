599dd4e5eeebe1420106437764e66397366dbcfb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 12:33:48 -0500

    Add form handling and TourInfo component to NewTour

--- components/NewTour.tsx
+++ components/NewTour.tsx
@@ -1,3 +1,43 @@
+'use client'
+
+import TourInfo from './TourInfo'
+
 export default function NewTour() {
-  return <div>NewTour</div>
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className='text-4xl font-bold'>New Tour</h2>
+
+        <div className='join w-full mt-6'>
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='city'
+            name='city'
+            required
+          />
+
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='country'
+            name='country'
+            required
+          />
+
+          <button className='btn btn-primary join-item' type='submit'>
+            Generate Tour
+          </button>
+        </div>
+      </form>
+
+      <div className='mt-16'>
+        <TourInfo />
+      </div>
+    </>
+  )
 }
