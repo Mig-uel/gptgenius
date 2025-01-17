35d209238793f51356aab9a266407faf3a23df9e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 16:14:10 -0500

    Add search functionality to ToursPage for filtering tours by city or country

--- components/ToursPage.tsx
+++ components/ToursPage.tsx
@@ -3,16 +3,40 @@
 import { getAllTours } from '@/utils/actions'
 import { useQuery } from '@tanstack/react-query'
 import { Spinner, ToursList } from '@/components'
+import { useState } from 'react'
 
 export default function ToursPage() {
-  const { data, isPending } = useQuery({
-    queryKey: ['tours'],
+  const [searchTerm, setSearchTerm] = useState('')
 
-    queryFn: () => getAllTours(),
+  const { data, isPending } = useQuery({
+    queryKey: ['tours', searchTerm],
+
+    queryFn: () => getAllTours(searchTerm),
   })
 
   return (
     <div className='mt-8'>
+      <form className='max-w-lg mb-12'>
+        <div className='join w-full'>
+          <input
+            type='text'
+            placeholder='enter city or country here...'
+            className='input input-bordered join-item w-full'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            required
+          />
+          <button
+            className='btn btn-primary join-item'
+            type='button'
+            disabled={isPending}
+            onClick={() => setSearchTerm('')}
+          >
+            {isPending ? <Spinner /> : 'Reset'}
+          </button>
+        </div>
+      </form>
+
       {/* @ts-expect-error fix prisma types later */}
       <>{isPending ? <Spinner /> : <ToursList data={data} />}</>
     </div>
