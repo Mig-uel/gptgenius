46cbe4e4ef665dd2936231e8472621ac73ca22c3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 15:45:18 -0500

    Implement server-side data fetching for ToursPage with React Query

--- app/(dashboard)/tours/page.tsx
+++ app/(dashboard)/tours/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Page</div>
+import { ToursPage } from '@/components'
+import { getAllTours } from '@/utils/actions'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
+
+export default async function Page() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['tours'],
+
+    queryFn: () => getAllTours(),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <h2 className='text-4xl font-bold'>Tours</h2>
+
+      <ToursPage />
+    </HydrationBoundary>
+  )
 }
