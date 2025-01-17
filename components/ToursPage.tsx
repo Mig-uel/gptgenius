0b00b4ddf51e5f34a0786d0f063b350f41a04c25
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-17 15:49:59 -0500

    Implement data fetching and loading state for ToursPage using React Query

--- components/ToursPage.tsx
+++ components/ToursPage.tsx
@@ -1,3 +1,16 @@
+'use client'
+
+import { getAllTours } from '@/utils/actions'
+import { useQuery } from '@tanstack/react-query'
+import { Spinner, ToursList } from '@/components'
+
 export default function ToursPage() {
-  return <div>ToursPage</div>
+  const { data, isPending } = useQuery({
+    queryKey: ['tours'],
+
+    queryFn: () => getAllTours(),
+  })
+
+  // @ts-expect-error fix prisma types later
+  return <>{isPending ? <Spinner /> : <ToursList data={data} />}</>
 }
