ff72d9e108a9d165375f26d94d1631394b384e14
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-19 11:40:17 -0500

    Fetch and display user tokens in profile page

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,5 +1,14 @@
+import { fetchUserTokensById } from '@/utils/actions'
 import { UserProfile } from '@clerk/nextjs'
 
-export default function Page() {
-  return <UserProfile />
+export default async function Page() {
+  const tokens = await fetchUserTokensById()
+
+  return (
+    <div>
+      <h2 className='mb-8 ml-8 text-xl font-extrabold'>Tokens: {tokens}</h2>
+
+      <UserProfile />
+    </div>
+  )
 }
