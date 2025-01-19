ccf520595a92d0cd24ad1b237a130a13b3450b21
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-19 09:56:35 -0500

    Add tour image generation to Tour detail page

--- app/(dashboard)/tours/[id]/page.tsx
+++ app/(dashboard)/tours/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { TourInfo } from '@/components'
-import { getTour } from '@/utils/actions'
+import { generateTourImage, getTour } from '@/utils/actions'
+import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import { FaArrowLeft } from 'react-icons/fa'
@@ -15,11 +16,26 @@ export default async function Page({
 
   if (!tour) return redirect('/tours')
 
+  const tourImage = await generateTourImage(tour)
+
   return (
     <div>
       <Link href='/tours' className='btn btn-secondary mb-12'>
         <FaArrowLeft /> Back to Tours
       </Link>
+
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            alt={tour.title}
+            width={300}
+            height={300}
+            className='rounded-xl shadow-xl mb-16 h-96 w-96 object-center'
+            priority
+          />
+        </div>
+      ) : null}
 
       <TourInfo tour={tour} />
     </div>
