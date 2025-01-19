357ae95adfa9c8aab5c48ced9e97cd83267ec7de
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-19 11:40:22 -0500

    Fetch or generate user tokens in MemberProfile component

--- components/MemberProfile.tsx
+++ components/MemberProfile.tsx
@@ -1,3 +1,4 @@
+import { fetchOrGenerateUserTokensById } from '@/utils/actions'
 import { currentUser, UserButton } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
@@ -5,6 +6,8 @@ export default async function MemberProfile() {
   const user = await currentUser()
 
   if (!user) return redirect('/')
+
+  await fetchOrGenerateUserTokensById()
 
   return (
     <div className='px-4 flex items-center gap-2'>
