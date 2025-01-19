8199b066a0d037218e9816b9eb4aae3e58dbc161
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-19 12:08:34 -0500

    Pass userId to fetchOrGenerateUserTokensById in MemberProfile component

--- components/MemberProfile.tsx
+++ components/MemberProfile.tsx
@@ -1,13 +1,14 @@
 import { fetchOrGenerateUserTokensById } from '@/utils/actions'
-import { currentUser, UserButton } from '@clerk/nextjs'
+import { auth, currentUser, UserButton } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
 export default async function MemberProfile() {
   const user = await currentUser()
+  const { userId } = auth()
 
   if (!user) return redirect('/')
 
-  await fetchOrGenerateUserTokensById()
+  await fetchOrGenerateUserTokensById(userId!)
 
   return (
     <div className='px-4 flex items-center gap-2'>
